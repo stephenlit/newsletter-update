1b81f315b3e021a86d22a8b7db1dce70a4009dea
Stephen Little <47438829+Stevish1968@users.noreply.github.com>
2024-02-21 22:56:16 -0400

    clone commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,37 @@
-import Header from './components/Header'
-import MainBody from './components/MainBody'
-import EmailInput from './components/EmailInput'
-import Thanks from './components/Thanks'
+import Header from './components/Header';
+import MainBody from './components/MainBody';
+import EmailInput from './components/EmailInput';
+import Thanks from './components/Thanks';
 
-import { useState } from 'react'
-
+import { useState } from 'react';
 
 function App() {
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [email, setEmail] = useState();
 
-  const [isSubmitted, setIsSubmitted] = useState(false)
-  const [email, setEmail] = useState()
-
-
-  return (
-    <div className='wrapper'>
-      {isSubmitted ? <Thanks email={email} setIsSubmitted={setIsSubmitted} setEmail={setEmail}/> :
-        <div className="container">
-          <Header />
-          <main>
-            <MainBody />
-            <EmailInput setIsSubmitted={setIsSubmitted} email={email} setEmail={setEmail}  />
-          </main>
-        </div>
-      }
-    </div >
-  )
+    return (
+        <section className='wrapper'>
+            {isSubmitted ? (
+                <Thanks
+                    email={email}
+                    setIsSubmitted={setIsSubmitted}
+                    setEmail={setEmail}
+                />
+            ) : (
+                <section className='container'>
+                    <Header />
+                    <main>
+                        <MainBody />
+                        <EmailInput
+                            setIsSubmitted={setIsSubmitted}
+                            email={email}
+                            setEmail={setEmail}
+                        />
+                    </main>
+                </section>
+            )}
+        </section>
+    );
 }
 
-export default App
+export default App;
