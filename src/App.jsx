7ccca0bb79daa36ed83335dc15a3966845c6d5a0
stephenlit <stevish45@icloud.com>
2024-02-23 11:43:06 -0400

    change double code in app.js file

--- src/App.jsx
+++ src/App.jsx
@@ -32,41 +32,6 @@ function App() {
       )}
     </section>
   );
-=======
-import Header from './components/Header';
-import MainBody from './components/MainBody';
-import EmailInput from './components/EmailInput';
-import Thanks from './components/Thanks';
-
-import { useState } from 'react';
-
-function App() {
-    const [isSubmitted, setIsSubmitted] = useState(false);
-    const [email, setEmail] = useState();
-
-    return (
-        <section className='wrapper'>
-            {isSubmitted ? (
-                <Thanks
-                    email={email}
-                    setIsSubmitted={setIsSubmitted}
-                    setEmail={setEmail}
-                />
-            ) : (
-                <section className='container'>
-                    <Header />
-                    <main>
-                        <MainBody />
-                        <EmailInput
-                            setIsSubmitted={setIsSubmitted}
-                            email={email}
-                            setEmail={setEmail}
-                        />
-                    </main>
-                </section>
-            )}
-        </section>
-    );
 }
 
 export default App;
